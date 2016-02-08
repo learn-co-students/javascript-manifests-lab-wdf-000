ad35409f9eadeee691799ca9a96187df5d5cb5e7
Blake Johnson <blake41@gmail.com>
2016-02-08 16:00:52 -0500

    spec updates

--- javascript_manifest_rails/spec/manifests/manifest_spec.rb
+++ javascript_manifest_rails/spec/manifests/manifest_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 RSpec.describe do
   it "should require the alert file" do
-    expect(File.read("app/assets/javascripts/application.js")).to include("//= alert_maker")
+    expect(File.read("app/assets/javascripts/application.js")).to include("//= require alert_maker")
+  end
+
+  it "should require the alert file" do
+    expect(File.read("app/assets/javascripts/application.js")).to include("//= require wrong_place_wrong_time")
+  end
+
+  it "should require the alert file" do
+    expect(File.read("app/assets/javascripts/application.js")).to include("//= require main")
   end
 end
