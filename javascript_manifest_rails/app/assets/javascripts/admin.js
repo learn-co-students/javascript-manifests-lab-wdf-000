a8fb9c12a5024158125ee729820ab0158882cca9
Blake Johnson <blake41@gmail.com>
2016-02-08 15:50:25 -0500

    solution

--- javascript_manifest_rails/app/assets/javascripts/admin.js
+++ javascript_manifest_rails/app/assets/javascripts/admin.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
-//= require alert_maker
+//= require hello_world
+//= require doesn_nothing
 //= require main
--- javascript_manifest_rails/app/assets/javascripts/learn.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $('body').append('<h3>Adding learning to this page</h3>');
-});
