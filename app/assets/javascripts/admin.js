76a195d0c29d6a32f913bde9d5176f03451173e5
ozPop <ozziepopovas@gmail.com>
2016-10-29 13:40:24 -0400

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
-//= require main
-//= require alert_maker
+//= require hello_world
+//= require doesn_nothing
