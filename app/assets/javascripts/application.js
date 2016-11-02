4364cd5ba3a43e9ab4634b4e0b50771a6605920b
paige11 <ellis.paige.v@gmail.com>
2016-11-02 16:16:06 -0400

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
 //= require main
 //= require alert_maker
+//= require wrong_place_wrong_time
