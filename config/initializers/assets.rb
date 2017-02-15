e81dabbfaab89e5eefceabf57e81cbf432568149
zenglue <keonilrath@gmail.com>
2017-02-15 14:57:52 -0500

    Done.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.precompile += %w( learn.js )
+Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( main.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
